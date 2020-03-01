43a8766d697aa04d8010947119b633edd123489c
soniaArenas <puente46191@gmail.com>
2020-03-01 18:31:44 +0100

    close #5

---
+++ FileManager/FileManager.DataAccess.Data/ConcretFactory/Json.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data
+{
+    class Json : IFileFactory
+    {
+        public ICreateFile GetCreateFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDeleteFile GetDeleteFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadFile GetReadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUpdateFile GetUpdateFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileManager/FileManager.DataAccess.Data/ConcretFactory/Xml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data
+{
+    class Xml:IFileFactory
+    {
+        public ICreateFile GetCreateFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadFile GetReadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUpdateFile GetUpdateFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDeleteFile GetDeleteFile()
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
