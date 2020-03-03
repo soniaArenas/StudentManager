5717b802502efcf0c8c6d5552203a32245c4517f
soniaArenas <puente46191@gmail.com>
2020-03-03 20:52:39 +0100

    Reflection. close #12

---
+++ FileManager/FileManager.DataAccess.Data/AbstractProduct/IFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Common.Layer;
+
+namespace FileManager.DataAccess.Data.AbstractProduct
+{
+    public abstract class IFile
+    {
+       public static SortedDictionary<int, Student> Students =
+            new SortedDictionary<int, Student>();
+       
+        public abstract void CreateFile(object student);
+        public abstract void ReadFile(object student);
+        public abstract void UpdateFile(object student);
+        public abstract void DeleteFile(object student);
+
+    }
+}
